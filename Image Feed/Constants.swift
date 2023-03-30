b5bc93db6c674abbbdb65d8e78f8d721a6179ca8
Pochemuchcka <dyakonoff.vladislav@yandex.ru>
2023-03-30 09:00:18 +0300

    no message

--- Image Feed/Constants.swift
+++ Image Feed/Constants.swift
@@ -12,3 +12,4 @@ let secretKey = "xuLEvN40RatVzkVPgvoAZZmoGNPMfW6lytv_h0Xq7XI"
 let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
 let accessScope = "public+read_user+write_likes"
 let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
